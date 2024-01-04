cf9cf3b37977a4daf740783818aef91a813a74b4
Moriom <chadninfo@gmail.com>
2024-01-05 05:54:16 +0600

    add: create classroom form

--- client/src/pages/Classroom/CreateClassroom.jsx
+++ client/src/pages/Classroom/CreateClassroom.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import PageLayout from '../../Layout/PageLayout';
+import { Box, Button, TextField } from '@mui/material';
 
 const CreateClassroom = () => {
     return (
-        <>
-            <h1>Create classroom page</h1>
-        </>
+        <PageLayout>
+            <Box sx={{ maxWidth: "450px", m: { md: "auto", xs: 2 }, p: 5, bgcolor: "white", borderRadius: 2, boxShadow: '0.65px 1.75px 10px rgb(0, 0, 0, 0.3)' }}>
+                <h5 className="mb-5 text-lg">Create classroom</h5>
+                <form className='flex flex-col gap-y-5'>
+                    <TextField
+                        id="component-classroom-name"
+                        label="Classroom name"
+                        multiline
+                        variant="standard"
+                        required
+                    />
+                    <TextField
+                        id="component-course"
+                        label="Course"
+                        multiline
+                        variant="standard"
+                        required
+                    />
+                    <TextField
+                        id="component-course-code"
+                        label="Course code"
+                        multiline
+                        variant="standard"
+                        required
+                    />
+                    <Button type='submit' className='self-end w-0' variant="text">Create</Button>
+                </form>
+            </Box>
+        </PageLayout>
     )
 }
 
