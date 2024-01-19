010c028a1c81604f9d2807d09e391d24170bdf16
Md. Ibrahim Hossain <ibuhossain3@gmail.com>
2024-01-19 17:43:36 +0600

    add: show member list in UI

--- client/src/pages/Classroom/ClassroomDetails.jsx
+++ client/src/pages/Classroom/ClassroomDetails.jsx
@@ -6,20 +6,27 @@ import axios from "axios";
 import { Dropdown } from '@mui/base/Dropdown';
 import { Menu } from '@mui/base/Menu';
 import { MenuButton } from '@mui/base/MenuButton';
-import { Button, Tooltip, FormControl, InputLabel, MenuItem, Select, ListSubheader } from '@mui/material';
+import { Button, Tooltip, FormControl, InputLabel, Box, Modal, MenuItem, Select, ListSubheader } from '@mui/material';
 import { styled } from '@mui/system';
 import { semesterList } from "../../utility/semesterList";
 import useUtility from "../../Hooks/useUtility";
 import { tagTitle } from "../../utility/tagTitle";
 
+const ModalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: "95%",
+    maxWidth: "450px",
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+};
+
 const ClassroomDetails = () => {
     const { rid } = useParams();
-    const { user, userDesignation } = useAuth();
-    const { getDepartments, deptLoading } = useUtility()
+    const { user } = useAuth();
     const [RoomInfo, setRoomInfo] = React.useState({});
-    const [requestEmail, setRequestEmail] = React.useState('')
-    const [semester, setSemester] = React.useState('')
-    const [department, setDepartment] = React.useState('')
 
     const handleFetchRoomDetails = () => {
         axios.get(`${import.meta.env.VITE_APP_BACKEND_WITHOUT_GQL}/classroom/${rid}`, {
@@ -30,6 +37,83 @@ const ClassroomDetails = () => {
             console.error(err)
         })
     }
+    console.log(RoomInfo)
+    React.useEffect(() => {
+        handleFetchRoomDetails();
+    }, []);
+
+    if (RoomInfo?.isJoined) {
+        return (
+            <PageLayout>
+                <div className="md:w-3/5 w-full m-auto">
+                    <RoomBanner RoomInfo={RoomInfo} />
+                    <div className="max-md:w-[95%] mx-auto">
+                        {RoomInfo?.members?.length > 0 && <ShowMembers RoomInfo={RoomInfo} />}
+                        <MemberAddingSection RoomInfo={RoomInfo} setRoomInfo={setRoomInfo} />
+                    </div>
+                </div>
+            </PageLayout>
+        )
+    } else {
+        return (
+            <PageLayout>
+                <div className="md:w-3/5 w-full m-auto">
+                    <RoomBanner RoomInfo={RoomInfo} />
+                </div>
+            </PageLayout>
+        )
+    }
+}
+
+const ShowMembers = ({ RoomInfo }) => {
+    const { user } = useAuth();
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    return (
+        <>
+            <div class="flex -space-x-3 rtl:space-x-reverse mb-5">
+                {
+                    RoomInfo?.members?.slice(0, 3)?.map((mem) => (
+                        <>
+                            <button onClick={handleOpen} class="w-8 h-8 border-2 border-gray-400 rounded-full bg-gray-300 hover:bg-gray-200 flex justify-center items-center text-sm font-medium text-center text-slate-700">TW</button>
+                        </>
+                    ))
+                }
+                {
+                    RoomInfo?.members?.length > 3 && <button onClick={handleOpen} class="flex items-center justify-center w-8 h-8 text-xs font-medium text-white bg-gray-700 border-2 border-white rounded-full hover:bg-gray-600 dark:border-gray-800" href="#">{RoomInfo?.members?.length - 3}+</button>
+                }
+            </div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+            >
+                <Box
+
+                    sx={{ ...ModalStyle, m: { md: "auto", xs: 'auto' }, p: 3, bgcolor: "white", borderRadius: 2, boxShadow: '0.65px 1.75px 10px rgb(0, 0, 0, 0.3)' }}
+                >
+                    <h3>Members</h3>
+                    <ul>
+                        <li>Bal</li>
+                        <li>Bal</li>
+                        <li>Bal</li>
+                        <li>Bal</li>
+                        <li>Bal</li>
+                        <li>Bal</li>
+                        <li>Bal</li>
+                    </ul>
+                </Box>
+            </Modal >
+        </>
+    )
+}
+
+const MemberAddingSection = ({ RoomInfo, setRoomInfo }) => {
+    const { user, userDesignation } = useAuth();
+    const { getDepartments, deptLoading } = useUtility()
+    const [requestEmail, setRequestEmail] = React.useState('')
+    const [semester, setSemester] = React.useState('')
+    const [department, setDepartment] = React.useState('')
 
     const handleAddSingleMember = (e) => {
         e.preventDefault();
@@ -64,109 +148,87 @@ const ClassroomDetails = () => {
             }
         })
     }
-    console.log(RoomInfo)
-    React.useEffect(() => {
-        handleFetchRoomDetails();
-    }, []);
 
-
-    if (RoomInfo?.isJoined) {
-        return (
-            <PageLayout>
-                <div className="md:w-3/5 w-full m-auto">
-                    <RoomBanner RoomInfo={RoomInfo} />
-                    {
-                        user?.email === RoomInfo?.admin?.email &&
-                        <div className="grid grid-cols-2">
-                            <div>
-                                <p className="mb-2">Add specific member:</p>
-                                <form onSubmit={handleAddSingleMember} className="flex items-center">
-                                    <input
-                                        type="text"
-                                        placeholder="Enter email"
-                                        name="requestEmail"
-                                        className="outline-none border-2 border-gray-200 rounded-lg px-2 py-1 rounded-e-none"
-                                        onChange={(e) => setRequestEmail(e.target.value)} />
-                                    <button type="submit"
-                                        className="rounded-lg rounded-s-none px-3 py-1 outline-none border-2 border-gray-200 border-s-0 hover:bg-sky-400 hover:border-sky-400 hover:text-white duration-200"
-                                    >ADD</button>
-                                </form>
-                            </div>
-                            {
-                                userDesignation === 'teacher' && <div>
-                                    <p className="mb-2">Add the students of:</p>
-                                    <form onSubmit={handleAddBulkMember} className="flex w-full justify-end items-stretch h-[36px]">
-                                        <FormControl size="small" fullWidth sx={{ height: '100%', mb: 2 }}>
-                                            <InputLabel id="department-selection">department</InputLabel>
-                                            <Select
-                                                value={department}
-                                                labelId="department-selection"
-                                                id="department-selection"
-                                                label="Department"
-                                                name="categories"
-                                                sx={{ height: "100%", borderRadius: 2, borderTopRightRadius: 0, borderEndEndRadius: 0 }}
-                                                onChange={(e) => setDepartment(e.target.value)}
-                                                required
-                                            >
-                                                {!deptLoading && getDepartments.map((item, index) => (
-                                                    item &&
-                                                    <MenuItem key={index} value={item}>
-                                                        <Tooltip title={tagTitle[item] || ''} placement="top-start" arrow>
-                                                            <div className="w-full">{item.toUpperCase()}</div>
-                                                        </Tooltip>
-                                                    </MenuItem>
-                                                ))}
-                                                <MenuItem value="others" sx={{ bgcolor: "orange", ":hover": { bgcolor: "skyblue" } }}>
-                                                    <Tooltip title="If not sure." placement="top-start" arrow>
-                                                        <div className="w-full">OTHERS</div>
-                                                    </Tooltip>
-                                                </MenuItem>
-                                            </Select>
-                                        </FormControl>
-                                        <FormControl fullWidth sx={{ height: '100%' }}>
-                                            <InputLabel size="small" htmlFor="semester-select">semester</InputLabel>
-                                            <Select
-                                                value={semester}
-                                                name="semester"
-                                                id="semester-select"
-                                                labelId="semester-select"
-                                                label="semester"
-                                                sx={{ height: "100%", borderRadius: 0 }}
-                                                onChange={(e) => setSemester(e.target.value)}
-                                                required
-                                            >
-                                                {
-                                                    semesterList.map((sem) => {
-                                                        if (sem?.title) {
-                                                            return (
-                                                                <ListSubheader key={sem.title} sx={{ fontWeight: "700" }}>{sem.title}</ListSubheader>
-                                                            )
-                                                        }
-                                                        return (
-                                                            <MenuItem key={sem} value={sem} sx={{ ml: 1 }}>{sem}</MenuItem>
-                                                        )
-                                                    })
-                                                }
-                                            </Select>
-                                        </FormControl>
-                                        <Button type="submit" sx={{ height: "100%", borderRadius: 2, borderTopLeftRadius: 0, borderEndStartRadius: 0, boxShadow: "none" }} variant="contained" >Add</Button>
-                                    </form>
-                                </div>
-                            }
-                        </div>
-                    }
+    return (
+        user?.email === RoomInfo?.admin?.email &&
+        <div className="grid lg:grid-cols-2 lg:gap-0 gap-5 grid-cols-1">
+            <div>
+                <p className="mb-2">Add specific member:</p>
+                <form onSubmit={handleAddSingleMember} className="flex items-center">
+                    <input
+                        type="text"
+                        placeholder="Enter email"
+                        name="requestEmail"
+                        className="outline-none border-2 border-gray-200 rounded-lg px-2 py-1 rounded-e-none max-lg:w-full"
+                        onChange={(e) => setRequestEmail(e.target.value)} />
+                    <button type="submit"
+                        className="rounded-lg rounded-s-none px-3 py-1 outline-none border-2 border-gray-200 border-s-0 hover:bg-sky-400 hover:border-sky-400 hover:text-white duration-200"
+                    >ADD</button>
+                </form>
+            </div>
+            {
+                userDesignation === 'teacher' && <div>
+                    <p className="mb-2">Add the students of:</p>
+                    <form onSubmit={handleAddBulkMember} className="flex w-full justify-end items-stretch h-[36px]">
+                        <FormControl size="small" fullWidth sx={{ height: '100%', mb: 2 }}>
+                            <InputLabel id="department-selection">department</InputLabel>
+                            <Select
+                                value={department}
+                                labelId="department-selection"
+                                id="department-selection"
+                                label="Department"
+                                name="categories"
+                                sx={{ height: "100%", borderRadius: 2, borderTopRightRadius: 0, borderEndEndRadius: 0 }}
+                                onChange={(e) => setDepartment(e.target.value)}
+                                required
+                            >
+                                {!deptLoading && getDepartments.map((item, index) => (
+                                    item &&
+                                    <MenuItem key={index} value={item}>
+                                        <Tooltip title={tagTitle[item] || ''} placement="top-start" arrow>
+                                            <div className="w-full">{item.toUpperCase()}</div>
+                                        </Tooltip>
+                                    </MenuItem>
+                                ))}
+                                <MenuItem value="others" sx={{ bgcolor: "orange", ":hover": { bgcolor: "skyblue" } }}>
+                                    <Tooltip title="If not sure." placement="top-start" arrow>
+                                        <div className="w-full">OTHERS</div>
+                                    </Tooltip>
+                                </MenuItem>
+                            </Select>
+                        </FormControl>
+                        <FormControl fullWidth sx={{ height: '100%' }}>
+                            <InputLabel size="small" htmlFor="semester-select">semester</InputLabel>
+                            <Select
+                                value={semester}
+                                name="semester"
+                                id="semester-select"
+                                labelId="semester-select"
+                                label="semester"
+                                sx={{ height: "100%", borderRadius: 0 }}
+                                onChange={(e) => setSemester(e.target.value)}
+                                required
+                            >
+                                {
+                                    semesterList.map((sem) => {
+                                        if (sem?.title) {
+                                            return (
+                                                <ListSubheader key={sem.title} sx={{ fontWeight: "700" }}>{sem.title}</ListSubheader>
+                                            )
+                                        }
+                                        return (
+                                            <MenuItem key={sem} value={sem} sx={{ ml: 1 }}>{sem}</MenuItem>
+                                        )
+                                    })
+                                }
+                            </Select>
+                        </FormControl>
+                        <Button type="submit" sx={{ height: "100%", borderRadius: 2, borderTopLeftRadius: 0, borderEndStartRadius: 0, boxShadow: "none" }} variant="contained" >Add</Button>
+                    </form>
                 </div>
-            </PageLayout>
-        )
-    } else {
-        return (
-            <PageLayout>
-                <div className="md:w-3/5 w-full m-auto">
-                    <RoomBanner RoomInfo={RoomInfo} />
-                </div>
-            </PageLayout>
-        )
-    }
+            }
+        </div>
+    )
 }
 
 const RoomBanner = ({ RoomInfo }) => {
@@ -201,23 +263,23 @@ const RoomBanner = ({ RoomInfo }) => {
 
 const Listbox = styled('ul')(
     ({ theme }) => `
-    font-size: 0.875rem;
-    box-sizing: border-box;
-    margin: 12px 0;
-    min-width: 100px;
-    border-radius: 12px;
-    overflow: auto;
-    outline: none;
-    background: '#fff';
-    border: 1px solid #fff;
-    color: red;
-    font-weight: 500;
-    text-align: center;
-    cursor: pointer;
-    box-shadow: 0px 4px 6px ${theme.palette.mode === 'dark' ? 'rgba(0,0,0, 0.50)' : 'rgba(0,0,0, 0.05)'
+            font-size: 0.875rem;
+            box-sizing: border-box;
+            margin: 12px 0;
+            min-width: 100px;
+            border-radius: 12px;
+            overflow: auto;
+            outline: none;
+            background: '#fff';
+            border: 1px solid #fff;
+            color: red;
+            font-weight: 500;
+            text-align: center;
+            cursor: pointer;
+            box-shadow: 0px 4px 6px ${theme.palette.mode === 'dark' ? 'rgba(0,0,0, 0.50)' : 'rgba(0,0,0, 0.05)'
         };
-    z-index: 1;
-    `,
+            z-index: 1;
+            `,
 );
 
 export default ClassroomDetails;
